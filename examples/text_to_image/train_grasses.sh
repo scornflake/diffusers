1cfd3d8ff968a7ae884b1ad96a0fef87ce42ce29
Neil <neil@shinywhitebox.com>
2024-09-12 20:30:07 +1200

    move data, remove images
    move to using lora

---
+++ examples/text_to_image/train_grasses.sh
@@ -0,0 +1,22 @@
+
+export MODEL_NAME="stabilityai/stable-diffusion-xl-base-1.0"
+export VAE_NAME="madebyollin/sdxl-vae-fp16-fix"
+export DATASET_NAME="scornflake/stable-diffusion-xl-nz-grasses"
+
+accelerate launch train_text_to_image_lora_sdxl.py \
+  --pretrained_model_name_or_path=$MODEL_NAME \
+  --pretrained_vae_model_name_or_path=$VAE_NAME \
+  --dataset_name=$DATASET_NAME \
+  --enable_xformers_memory_efficient_attention \
+  --resolution=512 --center_crop --random_flip \
+  --train_batch_size=1 \
+  --gradient_accumulation_steps=4 --gradient_checkpointing \
+  --max_train_steps=10000 \
+  --use_8bit_adam \
+  --learning_rate=1e-06 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --mixed_precision="fp16" \
+  --report_to="wandb" \
+  --validation_prompt="grass or flax" --validation_epochs 5 \
+  --checkpointing_steps=5000 \
+  --output_dir="sdxl-grass-model" \
+  --push_to_hub
